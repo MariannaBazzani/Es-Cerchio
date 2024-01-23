d78af8b70c85d9689246e4c78a72941f95f64eb4
MariannaBazzani <merionbaz@gmail.com>
2024-01-23 17:35:41 +0100

    Aggiungere i file di progetto.

---
+++ Es Cerchio/Cerchio.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Es_Cerchio
+{
+    internal class Cerchio
+    {
+        public int Raggio { get; set; }
+        
+        public double Area()
+        {
+            return Raggio * Raggio * Math.PI;
+        }
+
+        public double Perimetro()
+        {
+            return 2 * Math.PI * Raggio;
+        }
+    }
+}
---
+++ Es Cerchio/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Es_Cerchio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cerchio cerchio = new Cerchio();
+            Console.WriteLine("Inserisci raggio: ");
+            cerchio.Raggio = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Area: " + cerchio.Area());
+            Console.WriteLine("Perimetro: " + cerchio.Perimetro());
+        }
+    }
+}
